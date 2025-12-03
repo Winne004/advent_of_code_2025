eb9adeb6aeabe4238c6bc3fc8396bccdae09d5df
Jeremy Winchester <jeremy.c.winchester@gmail.com>
2025-12-03 16:41:31 +0000

    day 1 pt 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -24,6 +24,7 @@ class Safe:
     def __init__(self) -> None:
         self._dial_pos: int = 50
         self._dial_size: int = 100
+        self._number_of_rotations: int = 0
 
     def rotate(self, rotation: Rotation) -> None:
         if rotation.direction == "L":
@@ -33,24 +34,42 @@ class Safe:
 
         self._dial_pos %= self._dial_size
 
+    def calculate_rotations(self, rotation: Rotation) -> None:
+        clicks = 0
+        if rotation.direction == "L":
+            dial_pos = self._dial_pos - rotation.amount
+            clicks = ((self._dial_pos - 1) // 100) - ((dial_pos - 1) // 100)
+
+        if rotation.direction == "R":
+            dial_pos = self._dial_pos + rotation.amount
+            clicks = (dial_pos // 100) - (self._dial_pos // 100)
+
+        self._number_of_rotations += clicks
+
     @property
     def current_position(self) -> int:
         return self._dial_pos
+
+    @property
+    def number_of_rotations(self) -> int:
+        return self._number_of_rotations
 
 
 def pt_1() -> None:
     res = 0
     input_path = Path() / "day_1" / "input.txt"
     print(input_path.absolute())
+
     input = FileHandler(input_path, parser=DayOneInputParser())
     safe = Safe()
     for line in input.read_lines():
+        safe.calculate_rotations(line)
         safe.rotate(line)
-        print(safe.current_position)
         if safe.current_position == 0:
             res += 1
 
-    print(f"Result: {res}")
+    print(f"Number of rotations: {safe.number_of_rotations}")
+    print(f"Result: {res + safe.number_of_rotations}")
 
 
 if __name__ == "__main__":
